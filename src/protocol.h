f9f80630c33b7f5f1b2c8e4951a44b3a02e6a5b0
Press Tab <presstab1337@gmail.com>
2014-07-12 11:49:17 -0600

    Port fix

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 28775 : 18775;
+    return testnet ? 28775 : 18777;
 }
 
 
