a3bea7fe9475abe474bd786985834917f8aff234
Press Tab <presstab1337@gmail.com>
2014-08-02 15:21:20 -0600

    Version 1.0.4

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("HyperStake");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "HyperStake-1.0.3"
+#define CLIENT_VERSION_SUFFIX   "HyperStake-1.0.4"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
