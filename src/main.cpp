c6643eb0391ab101655a5de315bd861e13931949
presstab <presstab1337@gmail.com>
2014-10-21 08:50:54 -0600

    change nlimit & hashhighblock

--- src/main.cpp
+++ src/main.cpp
@@ -3281,7 +3281,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
         // Send the rest of the chain
         if (pindex)
             pindex = pindex->pnext;
-        int nLimit = 1250;
+        int nLimit = 5000;
         printf("getblocks %d to %s limit %d\n", (pindex ? pindex->nHeight : -1), hashStop.ToString().substr(0,20).c_str(), nLimit);
         for (; pindex; pindex = pindex->pnext)
         {
