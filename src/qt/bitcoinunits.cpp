3f5f4581c9326b0502c3ec943cfb4509c9f1eaa7
Press Tab <presstab1337@gmail.com>
2015-01-08 00:04:51 -0700

    Revert "format unit with "," for non-english locale"
    This reverts commit e71dd0de95bcc426a7210e92105ad477ea5864d5.

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -110,10 +110,7 @@ QString BitcoinUnits::format(int unit, qint64 n, bool fPlus)
         quotient_str.insert(0, '-');
     else if (fPlus && n > 0)
         quotient_str.insert(0, '+');
-    if(QLocale::system() == QLocale::English || QLocale::C)
-		return quotient_str + QString(".") + remainder_str;
-	else
-		return quotient_str + QString(",") + remainder_str;
+    return quotient_str + QString(".") + remainder_str;
 }
 
 QString BitcoinUnits::formatAge(int unit, qint64 n, bool fPlus)
