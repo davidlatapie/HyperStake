fb8020926f3041cfaab0fb7ae99561b596ac2d9c
Press Tab <presstab1337@gmail.com>
2015-01-24 10:23:22 -0700

    v 1.0.9.1 beta

--- src/version.h
+++ src/version.h
@@ -48,6 +48,6 @@ static const int MEMPOOL_GD_VERSION = 60002;
 #define DISPLAY_VERSION_MAJOR       1
 #define DISPLAY_VERSION_MINOR       0
 #define DISPLAY_VERSION_REVISION    9
-#define DISPLAY_VERSION_BUILD       0
+#define DISPLAY_VERSION_BUILD       1
 
 #endif
