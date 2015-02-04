3dd8204425949a94ead9e7ea48ffc7184304f4ab
RobVanMieghem <rob@vanmieghemcloud.com>
2015-02-04 07:00:13 +0100

    Autotool and fix some includes and definitions

--- src/version.h
+++ src/version.h
@@ -8,20 +8,6 @@
 #include <string>
 
 //
-// client versioning
-//
-
-static const int CLIENT_VERSION =
-                           1000000 * CLIENT_VERSION_MAJOR
-                         +   10000 * CLIENT_VERSION_MINOR
-                         +     100 * CLIENT_VERSION_REVISION
-                         +       1 * CLIENT_VERSION_BUILD;
-
-extern const std::string CLIENT_NAME;
-extern const std::string CLIENT_BUILD;
-extern const std::string CLIENT_DATE;
-
-//
 // network protocol versioning
 //
 
