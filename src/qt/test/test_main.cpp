0606e9acfb845c49dcca479112867925950e3dcd
Rob Van Mieghem <rob@vanmieghemcloud.com>
2015-03-16 08:11:25 +0100

    remove obsolete uritests

--- src/qt/test/test_main.cpp
+++ src/qt/test/test_main.cpp
@@ -1,16 +1,11 @@
 #include <QTest>
 #include <QObject>
 
-#include "uritests.h"
 
 // This is all you need to run all the tests
 int main(int argc, char *argv[])
 {
-    bool fInvalid = false;
-
-    URITests test1;
-    if (QTest::qExec(&test1) != 0)
-        fInvalid = true;
+    bool fInvalid = true;
 
     return fInvalid;
 }
