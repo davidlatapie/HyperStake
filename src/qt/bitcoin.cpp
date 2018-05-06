709ec0017a363b52574a35d994f2815502114031
presstab <presstab1337@gmail.com>
2018-05-06 14:45:45 -0600

    Fix splashscreen and icons.
    update to include .gifs

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -135,7 +135,7 @@ int main(int argc, char *argv[])
     QTextCodec::setCodecForCStrings(QTextCodec::codecForTr());
 	#endif
 	
-    //Q_INIT_RESOURCE(hyperstake);
+    Q_INIT_RESOURCE(bitcoin);
     
     QApplication app(argc, argv);
 #if QT_VERSION > 0x050100
